c2d863c315dcc2db6fb5873212ce3daf9b5b7f35
Nabid Nihal <102453803+hubbardcenaaa@users.noreply.github.com>
2023-04-11 12:40:57 +0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,96 +1,106 @@
-const menuToggle = document.getElementById('menu-toggle');
-const menuu = document.getElementById('menuu');
-
-menuToggle.addEventListener('click', function() {
-  menuu.classList.toggle('hidden');
-  menuToggle.classList.toggle('active');
-});
-
-
-// $('.about-content ul li a').on('click', function () {
-//   $('.about-content ul li a').removeClass('active');
-//   $(this).addClass('active');
-// });
-
-
-
-// $('.counter').counterUp({
-//   delay: 10,
-//   time: 2000
-// });
-
-
-
-// $(".counting_number").each(function () {
-//   $(this)
-//     .prop("Counter", 0)
-//     .animate(
-//       {
-//         Counter: $(this).text(),
-//       },
-//       {
-//         duration: 4000,
-//         easing: "swing",
-//         step: function (now) {
-//           $(this).text(Math.ceil(now));
-//         },
-//       }
-//     );
-// });
+$('#test').on('click',function(){
+    alert();
+})
 
 
 
 
-
-$(function () {
-
-  $(window).on('scroll', function () {
-      var scrollSize = $(window).scrollTop()
-      if (scrollSize > 500) {
-          $('#backtotop').slideDown()
-      } else {
-          $('#backtotop').slideUp()
-      }
-
-
-      if (scrollSize > 50) {
-          $('#navigation').addClass('active')
-      } else {
-          $('#navigation').removeClass('active')
-      }
-  })
-}) 
-  
-  $('#backtotop').on('click', function () {
-      $('html, body').animate({
-          scrollTop:0,
-      },1000)
-  })
+// $(selector).action() //jquery syntax
 
 
 
+$(function(){
+    $('#showBtn').on('click', function(){
+        $('#element').show(5000);
+    });
+
+    $('#hideBtn').on('click', function(){
+        $('#element').hide(5000);
+    })
+
+    
+    $('#toggleBtn').on('click', function(){
+        $('#element').toggle('slow');
+    })
+
+})
 
 
-// var mySlider = $('.pogoSlider').pogoSlider({
-//     pauseOnHover: false,
-//     generateButtons: false,
-//     generateNav: false
-// }).data('plugin_pogoSlider');
-// $('.arrow_left').on('click', function () {
-//     mySlider.prevSlide();
-// });
-// $('.arrow_right').on('click', function () {
-//     mySlider.nextSlide();
-// });
+
+$(function(){
+    $('#slideUp').on('click', function(){
+        $('#element').slideUp(2000);
+    });
+
+    $('#slideDown').on('click', function(){
+        $('#element').slideDown(2000);
+    })
+
+    
+    $('#slideToggle').on('click', function(){
+        $('#element').slideToggle('slow');
+    })
+
+})
+
+
+$(function(){
+    $('#fadeIn').on('click', function(){
+        $('#element').fadeIn(2000);
+    });
+
+    $('#fadeOut').on('click', function(){
+        $('#element').fadeOut(2000);
+    })
+
+    
+    $('#fadeToggle').on('click', function(){
+        $('#element').fadeToggle('slow');
+    })
+     
+    $('#fadeTo').on('click', function(){
+        $('#element').fadeTo(2000, 0.3).hide(1000).show(1000).slideUp(1000).slideDown(1000).fadeTo(2000,0.8);
+    })
+
+})
+
+
+
+$(function(){
+    $('#animateBtn').on('click',function(){
+        $('#element').animate({
+            width: '500px',
+            height: '500px',
+
+        },5000)
+    });
+    $('#stopBtn').on('click', function(){
+        $('#element').stop();
+    });
+
+    $('#animateAtn').on('click',function(){
+        $('#element').toggleClass('active');
+    });
+
+
+    $('#chaining').on('click',function(){
+        $('#element').css('background','green').slideUp(1000).slideDown(1000).hide(1000);
+    })
+
+    $('#showText').on('click',function(){
+        alert('Text: '+ $('#lorem').text());
+    });
+
+
+
+    $(window).on('load', function(){
+        $('#preloader').hide();
+    })
+})
 
 
 
 
-// $('.fade').slick({
-//   infinite: true,
-//   slidesToShow: 3,
-//   slidesToScroll: 3
-// });
 
 
 
